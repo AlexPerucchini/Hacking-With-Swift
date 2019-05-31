93c88fb64255ac5117766d8eee3367eb02d00ffa
Alex Perucchini <alessandrop65@msn.com>
2019-05-30 18:44:26 -0600

    Enemy spawn and timers

--- Whack-Pinguin-Project14/Whack-Pinguin-Project14/GameScene.swift
+++ Whack-Pinguin-Project14/Whack-Pinguin-Project14/GameScene.swift
@@ -19,6 +19,7 @@ class GameScene: SKScene {
             gameScore.text = "Score: \(score)"
         }
     }
+    var popupTime = 0.85
 
     override func didMove(to view: SKView) {
         
@@ -39,6 +40,8 @@ class GameScene: SKScene {
         for i in 0 ..< 4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 320)) }
         for i in 0 ..< 5 { createSlot(at: CGPoint(x: 100 + (i * 170), y: 230)) }
         for i in 0 ..< 4 { createSlot(at: CGPoint(x: 180 + (i * 170), y: 140)) }
+        
+        createEnemy()
 
     }
 
@@ -51,5 +54,27 @@ class GameScene: SKScene {
         slot.configure(at: position)
         addChild(slot)
         slots.append(slot)
+    }
+    
+    func createEnemy() {
+        popupTime *= 0.991 // make popuptime get faster and faster
+        
+        slots.shuffle()
+        slots[0].show(hideTime: popupTime)
+        // randomize which slot to show
+        if Int.random(in: 0...12) > 4 { slots[1].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 8 {  slots[2].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 10 { slots[3].show(hideTime: popupTime) }
+        if Int.random(in: 0...12) > 11 { slots[4].show(hideTime: popupTime)  }
+        
+        let minDelay = popupTime / 2.0
+        let maxDelay = popupTime * 2
+        let delay = Double.random(in: minDelay...maxDelay)
+        
+        // GCD grand central dispatch
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+            [weak self] in
+            self?.createEnemy()
+        }
     }
 }
--- Whack-Pinguin-Project14/Whack-Pinguin-Project14/WhackSlot.swift
+++ Whack-Pinguin-Project14/Whack-Pinguin-Project14/WhackSlot.swift
@@ -11,6 +11,9 @@ import SpriteKit
 
 class WhackSlot: SKNode {
     var charNode: SKSpriteNode!
+    var myLabel:  SKLabelNode!
+    var isVisible = false
+    var isHit = false
     
     func configure(at position: CGPoint) {
         self.position = position
@@ -24,11 +27,54 @@ class WhackSlot: SKNode {
         //cropNode.maskNode = nil
         cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
         
+        
         charNode = SKSpriteNode(imageNamed: "penguinGood")
         charNode.position = CGPoint(x: 0, y: -90)
         charNode.name = "character"
         cropNode.addChild(charNode)
         
+        let say = ["Ha-ha!", "Boo!", "Nope!", "Zilch!", "Nada!"]
+
+        myLabel = SKLabelNode(fontNamed: "Chalkduster")
+        myLabel.text = say.shuffled().first
+        myLabel.fontSize = 20
+        myLabel.position = CGPoint(x: 0, y: -80)
+    
+        cropNode.addChild(myLabel)
+        
         addChild(cropNode)
+    }
+    
+    func show(hideTime: Double) {
+        if isVisible { return }
+        
+        // move penguin up the slot
+        charNode.run(SKAction.moveBy(x: 0, y: 70, duration: 0.05))
+        myLabel.run(SKAction.moveBy(x:0,  y: 100, duration: 0.05))
+        isVisible = true
+        isHit = false
+        
+        if Int.random(in: 0...2) == 0 {
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
+
+            charNode.name = "charFriend"
+        } else {
+            charNode.texture = SKTexture(imageNamed: "penguinEvil")
+            charNode.name = "charEnemy"
+        }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) {
+            [weak self] in
+            self?.hide()
+        }
+    }
+    
+    func hide() {
+        if !isVisible { return }
+        // move penguin up the slot
+        charNode.run(SKAction.moveBy(x: 0, y: -70, duration: 0.05))
+        myLabel.run(SKAction.moveBy(x: 0, y: -100, duration: 0.05))
+        
+        isVisible = false
     }
 }
